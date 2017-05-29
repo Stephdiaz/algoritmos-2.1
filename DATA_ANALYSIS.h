e029ad0522188b0a1d11e7bf68a779c8d353da3a
Stephdiaz <diazstephany2009@hotmail.com>
2017-05-29 18:58:06 -0400

    renovado

--- DATA_ANALYSIS.h
+++ DATA_ANALYSIS.h
@@ -1,49 +1,15 @@
 #pragma once
-template <class tipo>		//todo lo hice de clase tipo, porque no se que valores en si recibe
 class DATA_ANALYSIS
 {
 private:
-	tipo _distancia;
-	tipo _velocidad:
-	tipo _peso;
-	tipo _fuerza;
-	tipo _aceleracion;
 	
 public:
-	DATA_ANALYSIS(void); //solo se trabajara en dos sistemas
-	void DATA_ANALYSIS(tipo, tipo, tipo, tipo, ...);
-	tipo Conversion_MKS (tipo distancia, tipo peso, tipo velocidad, tipo fuerza);	//no me acuerdo la libreria
-	tipo Conversion_CGS (tipo distancia, tipo peso, tipo fuerza,...);	//no me acuerdo la libreria
-
-	tipo Grafica_estadistica (tipo value1, tipo value2);			// pense en que la estadistica sea comparativa de dos movimientos y solo sea de un valor tiempo-tiempo distancia-distancia fuerza-fuerza
-
-	tipo Descripcion_mov (string codigo);							// Si se reconoce un movimiento, es decir nosotros hacemos que sea por ejemplo caida libre, que cada movimiento tenga un codigo, y se le muestre una pequeña descripcion de ese movimiento
-	string Mostrar_Valores_cte ();			// los valores constante que se utilice, aun que creo solo es gravedad, pero mostrar su valor
-	
+	DATA_ANALYSIS(void); 
+	tipo Promedio_columna (tipo, tipo, tipo, ...);
+	tipo Variacion_tiempo (string tiempo1, string tiempo2);	//dijistes que tiempo es fijo. cualquier variacion Velocidad aceleracion distancia, todo con relacion al tiempo
+	tipo Graficar_promedios (tipo, tipo, tipo, tipo);	// nose si esto es posuible en formulario ya que no es una estadistica en si, si no es como una grafica que sube y baja de acuerdo al valor
+	tipo Graficas_comparativas (tipo Value1, tipo Value 2);	// grafica entre dos movimientos
+	string Descrpcion (string codigo);	//esto de que cada mov tenga un codigo y en caso se proporcione ese codigo, se de una descrpcion teorica del mov
+	void Mostrar_datos_manuales (tipo, tipo, tipo,...);	//si mal no entendi, ellos mismos pueden meter sus datos, que estos se muestren y con eso se llama a la funcion de grafica
+	void Mostrar_velocidadmax (tipo a [][], int cantidad);
 };
-template <class tipo>	
-	DATA_ANALYSIS <tipo>::DATA_ANALYSIS()
-	{
-		_distancia = 0;
-		_velocidad = 0;
-		_peso = 0;
-		_fuerza = 0;
-		_aceleracion = 0;
-	}
-
-template <class tipo>
-	void DATA_ANALYSIS(tipo d, tipo v, tipo p, tipo f, tipo a)
-	{
-		_distancia = d;
-		_velocidad = v;
-		_peso = p;
-		_fuerza = f;
-		_aceleracion = a;
-	}
-
-template <class tipo>	
-tipo DATA_ANALYSIS <tipo>:: Conversion_MKS(tipo distancia, tipo peso, tipo velocidad, tipo fuerza,...)
-{
-
-}
-// use tres puntos porque por ahi es necesario algun otro dato
